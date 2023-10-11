46377b31da03bba96592db8f768982c4f266cfb8
Gabe Denton <mason.g.denton@vanderbilt.edu>
2023-10-11 14:38:37 -0500

    edit

--- webscraper/src/image_scraper.py
+++ webscraper/src/image_scraper.py
@@ -1,6 +1,5 @@
 import os
 import pyautogui
-from PIL import Image
 import pytesseract
 import time
 
@@ -9,13 +8,18 @@ if os.name == 'nt':  # This checks if the OS is Windows.
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def capture_and_log():
+    # Capture the screenshot using pyautogui
     screenshot = pyautogui.screenshot()
-    text = pytesseract.image_to_string(screenshot)
+    # Convert the screenshot to bytes
+    screenshot_bytes = screenshot.tobytes()
+    # Use pytesseract to extract text from the bytes
+    text = pytesseract.image_to_string(screenshot_bytes)
     # Filtering lines that contain ".com"
     domains = [line for line in text.split('\n') if '.com' in line]
     # Writing filtered lines to a log file
     with open('domains_log.txt', 'a') as f:
         for domain in domains:
+            print(domain)
             f.write(domain + '\n')
 
 for _ in range(10):  # Run 10 times
