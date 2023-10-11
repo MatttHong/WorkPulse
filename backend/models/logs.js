d03402957d4e0a648a0379b132ceb0a915b21568
Gabe Denton <mason.g.denton@vanderbilt.edu>
2023-10-11 15:09:39 -0500

    lskdjf:wq

--- backend/models/logs.js
+++ backend/models/logs.js
@@ -5,6 +5,8 @@ const logSchema = mongoose.Schema({
     employee : { type: String, required: true },
     task : { type: String, required: true },
     department : { type: [String], default: [], required: true},
+    startTimestamp : { type: Date, default: Date.now(), required: true },
+    endTimestamp : { type: Date, required: false },
     status : { type: String, default: [], required: true },
     log : [{
         string: { type: String, required: true },
@@ -13,5 +15,25 @@ const logSchema = mongoose.Schema({
     }] 
 
 });
+
+logSchema.methods.endSession = function () {
+    return new Promise(async (resolve, reject) => {
+      const end = Date.now(); 
+      if (this.startTimestamp < end && this.endTimestamp == null) {
+        try {
+          await this.model('Log').updateOne(
+            { _id: this._id }, 
+            { $set: { endTimestamp: end } } 
+          );
+          resolve();
+        } catch (error) {
+          reject(error); 
+        }
+      } else {
+        resolve(); 
+      }
+    });
+  };
+  
 
 module.exports = mongoose.model("Log", logSchema)
