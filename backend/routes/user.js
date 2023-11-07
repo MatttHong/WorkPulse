b4899f1adebb562c51a7492138fe1b4904195c25
Gabe Denton <mason.g.denton@vanderbilt.edu>
2023-11-07 11:18:43 -0600

    cleaning weird code

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
-const checkAuth = require("../utils/check-auth");
 const { validateSession } = require('../controllers/session'); 
 
 // router.use(validateSession);
