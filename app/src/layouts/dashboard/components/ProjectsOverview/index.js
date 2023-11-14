4259bfff542147da6550ab59f6ebd6ca742cceba
yigitdznloglu <yigit.duzenlioglu@vanderbilt.edu>
2023-11-13 20:21:12 -0600

    working on session overview

--- app/src/layouts/dashboard/components/ProjectsOverview/index.js
+++ app/src/layouts/dashboard/components/ProjectsOverview/index.js
@@ -1,19 +1,19 @@
 /**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
+ =========================================================
+ * Material Dashboard 2 React - v2.2.0
+ =========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+ * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+ * Copyright 2023 Creative Tim (https://www.creative-tim.com)
 
-Coded by www.creative-tim.com
+ Coded by www.creative-tim.com
 
  =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
 
-import { useState, useCallback } from "react";
+import {useState, useCallback, useEffect} from "react";
 
 // @mui material components
 import Dialog from "@mui/material/Dialog";
@@ -37,123 +37,102 @@ import DataTable from "examples/Tables/DataTable";
 // Data
 import data from "layouts/dashboard/components/ProjectsOverview/data";
 import AddProjectComponent from "./addProject";
+import axios from "axios";
 
 
 function Projects() {
-  const { columns, rows } = data();
-  const [menu, setMenu] = useState(null);
-  const [openDialog, setOpenDialog] = useState(false);
-  
+    const {columns, rows} = data();
+    const [menu, setMenu] = useState(null);
+    const [openDialog, setOpenDialog] = useState(false);
 
-  const openMenu = ({ currentTarget }) => setMenu(currentTarget);
-  const closeMenu = () => setMenu(null);
+    useEffect(() => {
+        const fetchLogData = async () => {
+            try {
+                const token = localStorage.getItem("token");
+                const id = localStorage.getItem("id");
+                const response = await axios.get(`http://localhost:3000/api/log/${id}`, {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                    }
+                });
 
-  const handleOpenDialog = () => {
+                if (response.data.status === "Success") {
 
-    setOpenDialog(true);
-    closeMenu();
-  };
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-  };
-  const renderMenu = (
-    <Menu
-      id="simple-menu"
-      anchorEl={menu}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "left",
-      }}
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={Boolean(menu)}
-      onClose={closeMenu}
-    >
-      <MenuItem onClick={handleOpenDialog}>New Project</MenuItem>
-    </Menu>
-  );
-  
-  const handleAddProject = async (projectData) => {
-    const BACKEND_ENDPOINT = 'http://localhost:3000/api/projects';
-    
-    try {
-        const response = await fetch(BACKEND_ENDPOINT, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(projectData)
-        });
-  
-        if (!response.ok) {
-          throw new Error('Network response was not ok: ' + response.statusText);
-        }
-        const data = await response.json();
-        console.log('Project added:', data);
-        // Close dialog and refresh projects list
-        handleCloseDialog();
-        // TODO: Refresh projects list here
-    } catch (error) {
-        console.error('Error adding project:', error);
-    }
-  };
-  
+                }
+            } catch (error) {
+                console.error("Failed to fetch user data:", error);
+            }
+        };
 
-  return (
-    <Card>
-      <MDBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
-        <MDBox>
-          <MDTypography variant="h6" gutterBottom>
-            Projects
-          </MDTypography>
-          <MDBox display="flex" alignItems="center" lineHeight={0}>
-            <Icon
-              sx={{
-                fontWeight: "bold",
-                color: ({ palette: { info } }) => info.main,
-                mt: -0.5,
-              }}
-            >
-              done
-            </Icon>
-            <MDTypography variant="button" fontWeight="regular" color="text">
-              &nbsp;<strong>30 done</strong> this month
-            </MDTypography>
-          </MDBox>
-        </MDBox>
-        <MDBox color="text" px={2}>
-          <Icon sx={{ cursor: "pointer", fontWeight: "bold" }} fontSize="small" onClick={openMenu}>
-            more_vert
-          </Icon>
-        </MDBox>
-        {renderMenu}
-      </MDBox>
-      <MDBox>
-        <DataTable
-          table={{ columns, rows }}
-          showTotalEntries={false}
-          isSorted={false}
-          noEndBorder
-          entriesPerPage={false}
-        />
-      </MDBox>
-      <Dialog open={openDialog} onClose={handleCloseDialog}>
-        <DialogTitle>Add New Project</DialogTitle>
-        <DialogContent>
-          <AddProjectComponent onAddProject={handleAddProject} />        
-        </DialogContent>
-        <MDButton onClick={handleCloseDialog} color="error">
-        Cancel
-        </MDButton>
-      </Dialog>
-      <DialogActions>
-  
+        fetchUserData();
+    }, []); // The empty array means this effect will only run once, similar to componentDidMount
 
-</DialogActions>
-    </Card>
-  );
+    const openMenu = ({currentTarget}) => setMenu(currentTarget);
+    const closeMenu = () => setMenu(null);
+
+    const handleOpenDialog = () => {
+
+        setOpenDialog(true);
+        closeMenu();
+    };
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+    const renderMenu = (
+        <Menu
+            id="simple-menu"
+            anchorEl={menu}
+            anchorOrigin={{
+                vertical: "top",
+                horizontal: "left",
+            }}
+            transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+            }}
+            open={Boolean(menu)}
+            onClose={closeMenu}
+        >
+            <MenuItem onClick={handleOpenDialog}>New Project</MenuItem>
+        </Menu>
+    );
+
+
+
+    return (
+        <Card>
+            <MDBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
+                <MDBox>
+                    <MDTypography variant="h6" gutterBottom>
+                        Sessions
+                    </MDTypography>
+                    <MDBox display="flex" alignItems="center" lineHeight={0}>
+                        <Icon
+                            sx={{
+                                fontWeight: "bold",
+                                color: ({palette: {info}}) => info.main,
+                                mt: -0.5,
+                            }}
+                        >
+                            done
+                        </Icon>
+                        <MDTypography variant="button" fontWeight="regular" color="text">
+                            &nbsp;<strong>30 done</strong> this month
+                        </MDTypography>
+                    </MDBox>
+                </MDBox>
+            </MDBox>
+            <MDBox>
+                <DataTable
+                    table={{columns, rows}}
+                    showTotalEntries={false}
+                    isSorted={false}
+                    noEndBorder
+                    entriesPerPage={false}
+                />
+            </MDBox>
+        </Card>
+    );
 }
 
 export default Projects;
