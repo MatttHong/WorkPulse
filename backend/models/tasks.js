4998bba3e8bae17648e79cd8566a30845cc4eae4
Gabe Denton <mason.g.denton@vanderbilt.edu>
2023-12-04 10:50:38 -0600

    fixing with rollback again

--- backend/models/tasks.js
+++ backend/models/tasks.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
+const Status = require('../utils/status');
+
 
 const taskSchema = mongoose.Schema({
 
     taskName : { type: String, required: true },
     taskAdministrators : { type: [String], required: true },
     employees : { type: [String], default: [], required: true},
-    status : { type: String, default: "", required: true },
-
+    status: {
+        type: String,
+        required: true,
+        enum: Object.values(Status),
+        default: Status.active
+    },
 });
 
 module.exports = mongoose.model("Task", taskSchema)
